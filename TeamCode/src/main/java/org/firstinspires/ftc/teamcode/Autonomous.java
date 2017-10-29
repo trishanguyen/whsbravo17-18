f76d1874ce4d57edd01b591bc8f20a87f8c0bff6
bk3468 <bk3468@gmail.com>
2017-10-29 11:21:33 -0700

    Added the omniDrive method

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
@@ -31,6 +31,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.sun.tools.javac.code.Attribute;
 
 /**
  * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
@@ -49,15 +50,88 @@ public class Autonomous {
 
     // Declare OpMode members.
     private static ElapsedTime runtime = new ElapsedTime();
-    private static DcMotor leftDrive = null;
-    private static DcMotor rightDrive = null;
+    public static final int right = 1;
+    public static final int left = 2;
+    public static final int forward = 3;
+    public static final int backward = 4;
+    // Diagonal Right Forward
+    public static final int dRF = 5;
+    // Diagonal Right Backward
+    public static final int dRB = 6;
+    // Diagonal Left Forward
+    public static final int dLF = 7;
+    // Diagonal Left Backward
+    public static final int dLB = 8;
+    // Left Motors
+    private static DcMotor MOne = null;
+    private static DcMotor MTwo = null;
+    // Right Motors
+    private static DcMotor MThree = null;
+    private static DcMotor MFour = null;
     //test
-    //public static void omni
+    public static void omniDrive(double power, byte direction, int time){
+        switch (direction) {
+            case right:
+                MOne.setPower(-power);
+                MTwo.setPower(power);
+                MThree.setPower(power);
+                MFour.setPower(-power);
+                break;
+            case left:
+                MOne.setPower(power);
+                MTwo.setPower(-power);
+                MThree.setPower(-power);
+                MFour.setPower(power);
+                break;
+            case forward:
+                MOne.setPower(power);
+                MTwo.setPower(power);
+                MThree.setPower(-power);
+                MFour.setPower(-power);
+                break;
+            case backward:
+                MOne.setPower(-power);
+                MTwo.setPower(-power);
+                MThree.setPower(power);
+                MFour.setPower(power);
+                break;
+            case dRF:
+                MOne.setPower(0);
+                MTwo.setPower(power);
+                MThree.setPower(0);
+                MFour.setPower(-power);
+                break;
+            case dRB:
+                MOne.setPower(0);
+                MTwo.setPower(-power);
+                MThree.setPower(0);
+                MFour.setPower(power);
+                break;
+            case dLF:
+                MOne.setPower(power);
+                MTwo.setPower(0);
+                MThree.setPower(-power);
+                MFour.setPower(0);
+                break;
+            case dLB:
+                MOne.setPower(-power);
+                MTwo.setPower(0);
+                MThree.setPower(power);
+                MFour.setPower(0);
+                break;
+
+            default:return;
+
+        }
+        chersTimer(time);
+    }
 
     public static void drive(double power, int time)
     {
-        leftDrive.setPower(power);
-        rightDrive.setPower(power);
+        MOne.setPower(power);
+        MTwo.setPower(power);
+        MThree.setPower(power);
+        MFour.setPower(power);
 
         chersTimer(time);
 
@@ -70,8 +144,11 @@ public class Autonomous {
     public static void turnLeft(double power, int time)
     {
 
-        leftDrive.setPower(-power);
-        rightDrive.setPower(power);
+        MOne.setPower(-power);
+        MTwo.setPower(-power);
+        MThree.setPower(power);
+        MFour.setPower(power);
+
         chersTimer(time);
         stopDriving();
     }
@@ -79,8 +156,11 @@ public class Autonomous {
     public static void turnRight(double power, int time)
     {
 
-        leftDrive.setPower(power);
-        rightDrive.setPower(-power);
+        MOne.setPower(power);
+        MTwo.setPower(power);
+        MThree.setPower(-power);
+        MFour.setPower(-power);
+
 
         chersTimer(time);
         stopDriving();
